e2a974b6bf47ae29ce8d9b9c8429ddaf65667937
MrDocx <dossantos.dany3@gmail.com>
2016-03-08 10:01:22 +0100

    Carré OK avec zoom osé

--- WINK/ViewController.swift
+++ WINK/ViewController.swift
@@ -25,12 +25,10 @@ class ViewController: UIViewController, UIScrollViewDelegate {
         // Create an obstacle view and add it to the scroll view for testing purposes
         let obstacleView = UICarreView(vm:self)
         
-        
         //obstacleView.backgroundColor = UIColor.clearColor()
         
         obstacleView.backgroundColor = UIColor.redColor();
         scrollView.addSubview(obstacleView)
-        
         
     }
 
